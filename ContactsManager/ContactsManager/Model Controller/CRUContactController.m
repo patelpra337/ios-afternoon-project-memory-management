a60ed57e36d581a54dfe2444ff66785250d63e48
Pravin Patel <patelpra337@gmail.com>
2020-08-12 20:21:34 -0500

    Update CRUContactController

--- ContactsManager/ContactsManager/Model Controller/CRUContactController.m
+++ ContactsManager/ContactsManager/Model Controller/CRUContactController.m
@@ -7,7 +7,49 @@
 //
 
 #import "CRUContactController.h"
+#import "CRUContact.h"
+
+@interface CRUContactController () {
+    NSMutableArray *_internalContacts;
+}
+
+@end
 
 @implementation CRUContactController
+
+- (instancetype)init
+{
+    if (self = [super init]) {
+        _internalContacts = [[NSMutableArray alloc] init];
+        [self addTestData];
+    }
+    return self;
+}
+
+- (void)addTestData
+{
+    CRUContact *contact1 = [[CRUContact alloc] initWithName:@"Pravin Patel" Email:@"pravin.patel@email.com" PhoneNumber:@"123-456-7890"];
+    
+    [_internalContacts addObject:contact1];
+    [contact1 release];
+}
+
+- (NSArray<CRUContact *> *)contacts
+{
+    return [_internalContacts.copy autorelease];
+}
+
+- (void)dealloc
+{
+    [_internalContacts release];
+    [super dealloc];
+}
+
+
+
+
+
+
+
 
 @end
